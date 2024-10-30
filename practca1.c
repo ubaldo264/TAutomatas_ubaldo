c4171469eb5a20d0e8845977ac86e90be4a80959
ubaldo264 <ubaldo-usg@hotmail.com>
2024-10-30 08:24:25 -0600

    Create practca1.c

---
+++ practca1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void letra_U() {
+    printf("U\n");
+}
+
+void letra_B() {
+    printf("b\n");
+}
+
+void letra_A() {
+    printf("a\n");
+}
+
+void letra_L() {
+    printf("l\n");
+}
+
+void letra_D() {
+    printf("d\n");
+}
+
+void letra_O() {
+    printf("o\n");
+}
+
+void mostrar_nombre() {
+    letra_U();
+    letra_B();
+    letra_A();
+    letra_L();
+    letra_D();
+    letra_O();
+}
+
+int main() {
+    mostrar_nombre();
+    return 0;
+}
